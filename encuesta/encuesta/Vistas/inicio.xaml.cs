347906c4c03feb592fe98e3c50f0b774f520631a
Maximiliano Sarlija <max.sarlija@outlook.com.ar>
2018-01-05 16:42:20 -0300

    Inicio
    Inicio + Btns Nueva encuesta - ver encuestas - tareas

---
+++ encuesta/encuesta/Vistas/inicio.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace encuesta.Vistas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class inicio : ContentPage
+	{
+		public inicio ()
+		{
+			InitializeComponent ();
+		}
+
+       //Accion Boton Nueva Encuesta
+        async void btnNuevaencuesta(object sender, EventArgs e)
+        {
+
+        }
+
+        //Accion Boton Ver Encuestas
+        async void btnVerencuestas(object sender, EventArgs e)
+        {
+
+        }
+
+        //Accion Boton Tareas
+        async void btnTareas(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
