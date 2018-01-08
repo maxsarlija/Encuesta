6a76f32723e8c5fd39df28594aa927de2ab7754b
Maximiliano Sarlija <max.sarlija@outlook.com.ar>
2018-01-08 15:28:49 -0300

    Vistas encuestas
    Creadas + notas

---
+++ encuesta/encuesta/Vistas/encuestas/encuestaDinaminadellenadodelaplanilla.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace encuesta.Vistas.encuestas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class encuestaDinaminadellenadodelaplanilla : ContentPage
+	{
+		public encuestaDinaminadellenadodelaplanilla ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ encuesta/encuesta/Vistas/evolucionVisita.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace encuesta.Vistas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class evolucionVisita : ContentPage
+	{
+		public evolucionVisita ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
