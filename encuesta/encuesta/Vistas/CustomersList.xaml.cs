c033e4b4320f1d3b23293e1717466470f257b1d9
Nacho <ig.gonzalez@live.com>
2018-02-15 20:20:24 +0000

    V 1.02
    Clientes con buscador.

--- encuesta/encuesta/Vistas/CustomersList.xaml.cs
+++ encuesta/encuesta/Vistas/CustomersList.xaml.cs
@@ -1,24 +1,54 @@
-﻿
+﻿using System.Linq;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.ComponentModel;
+using System;
 
 namespace encuesta.Vistas
 {
-    public partial class CustomersList : ContentPage
+    public partial class CustomersList : ContentPage, INotifyPropertyChanged
     {
-        private List<Customer> _customers;
+        private string _searchedText;
+        public string SearchedText
+        {
+            get { return _searchedText; }
+            set { _searchedText = value; OnPropertyChanged(); }
+        }
+
+        protected Database DB { get; set; }
+        private ObservableCollection<Customer> _customerCollection;
+
+        protected ObservableCollection<Customer> CustomerCollection
+        {
+            get
+            {
+                return _customerCollection;
+            }
+            set
+            {
+                _customerCollection = value;
+                OnPropertyChanged();
+            }
+        }
 
 
         public CustomersList()
         {
             InitializeComponent();
 
-            var database = new Database("Encuesta");
+            DB = new Database("Encuesta");
 
 
-            _customers = database.Query<Customer>("SELECT * FROM Customer");
-            CustomersListView.ItemsSource = _customers;
+            CustomerCollection = new ObservableCollection<Customer>();
+
+            foreach (var item in DB.GetItems<Customer>())
+            {
+                CustomerCollection.Add(item);
+            }
+
+            CustomersListView.ItemsSource = CustomerCollection;
         }
 
 
@@ -30,11 +60,32 @@ namespace encuesta.Vistas
                 return; //ItemSelected is called on deselection, which results in SelectedItem being set to null
             }
 
-            await Navigation.PushAsync(new Vistas.CustomerSurveys((Customer) e.SelectedItem));
+            await Navigation.PushAsync(new Vistas.CustomerSurveys((Customer)e.SelectedItem));
 
             ((ListView)sender).SelectedItem = null;
         }
+        
 
+        private void SearchBar_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            CustomersListView.BeginRefresh();
+
+            if (string.IsNullOrWhiteSpace(e.NewTextValue))
+                CustomersListView.ItemsSource = CustomerCollection;
+            else
+            {
+                CustomerCollection.Clear();
+                foreach (var item in DB.GetItems<Customer>().Where(c => c.Name.ToLower().Contains(e.NewTextValue.ToLower()) || 
+                                                                        c.Address.ToLower().Contains(e.NewTextValue.ToLower())))
+                {
+                    CustomerCollection.Add(item);
+                }
+
+                CustomersListView.ItemsSource = CustomerCollection;
+            }
+
+            CustomersListView.EndRefresh();
+        }
 
 
     }
--- encuesta/encuesta/Vistas/NewSurvey_SelectCustomer.xaml.cs
+++ encuesta/encuesta/Vistas/NewSurvey_SelectCustomer.xaml.cs
@@ -2,23 +2,51 @@
 using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Collections.ObjectModel;
 
 namespace encuesta.Vistas
 {
     public partial class NewSurvey_SelectCustomer : ContentPage
     {
-        private List<Customer> _customers;
+        private string _searchedText;
+        public string SearchedText
+        {
+            get { return _searchedText; }
+            set { _searchedText = value; OnPropertyChanged(); }
+        }
 
+        protected Database DB { get; set; }
+        private ObservableCollection<Customer> _customerCollection;
+
+        protected ObservableCollection<Customer> CustomerCollection
+        {
+            get
+            {
+                return _customerCollection;
+            }
+            set
+            {
+                _customerCollection = value;
+                OnPropertyChanged();
+            }
+        }
 
         public NewSurvey_SelectCustomer()
         {
             InitializeComponent();
 
-            var database = new Database("Encuesta");
+
+            DB = new Database("Encuesta");
 
 
-            _customers = database.Query<Customer>("SELECT * FROM Customer");
-            CustomersListView.ItemsSource = _customers;
+            CustomerCollection = new ObservableCollection<Customer>();
+
+            foreach (var item in DB.GetItems<Customer>())
+            {
+                CustomerCollection.Add(item);
+            }
+
+            CustomersListView.ItemsSource = CustomerCollection;
         }
 
 
@@ -37,7 +65,27 @@ namespace encuesta.Vistas
             ((ListView)sender).SelectedItem = null;
         }
 
-        
+
+        private void SearchBar_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            CustomersListView.BeginRefresh();
+
+            if (string.IsNullOrWhiteSpace(e.NewTextValue))
+                CustomersListView.ItemsSource = CustomerCollection;
+            else
+            {
+                CustomerCollection.Clear();
+                foreach (var item in DB.GetItems<Customer>().Where(c => c.Name.ToLower().Contains(e.NewTextValue.ToLower()) ||
+                                                                        c.Address.ToLower().Contains(e.NewTextValue.ToLower())))
+                {
+                    CustomerCollection.Add(item);
+                }
+
+                CustomersListView.ItemsSource = CustomerCollection;
+            }
+
+            CustomersListView.EndRefresh();
+        }
 
     }
 }
